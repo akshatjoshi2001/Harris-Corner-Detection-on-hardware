4787ddfbd06876c31369d66ad30bb305e4208c6c
Sarthak-22 <sarthakvora22@gmail.com>
2021-12-01 23:21:39 +0530

    Update 2 harrisscore

--- harris_score.v
+++ harris_score.v
@@ -1,18 +1,20 @@
 module(input [7:0] Gx[0:3][0:3], input clk, input [7:0] Gy[0:3][0:3], output [32:0] R, output rdy);
 
 reg [7:0] Matrix[0:1][0:1];
-wire [8:0]  det, trace;
+wire [15:0]  det
+wire [15:0] trace;
 
 
 genvar i;
 genvar j;
+parameter k = 0.1;
 
 always@(*) begin
 Matrix[0][0] <= 2'd0;
 Matrix[0][1] <= 2'd0;
 Matrix[1][0] <= 2'd0;
 Matrix[1][1] <= 2'd0;
-
+rdy <= 0;
 
 for (i=2'd0; i<=2'd3; i=i+2'd1) begin
   for (j=2'd0; j<=2'd3; j=j+2'd1) begin
@@ -28,9 +30,8 @@ trace <= Matrix[0][0] + Matrix[1][1];
 end
 
 always@(posedge clk) begin
-R <= 
+R <= (det - trace*trace);
 rdy <= 'd1;
-  
 end
 
 endmodule
