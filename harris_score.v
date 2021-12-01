e382fe0c8bffc41b672ec54b88b2100341951821
Sarthak-22 <sarthakvora22@gmail.com>
2021-12-01 21:56:44 +0530

    Added Harris Score

--- harris_score.v
+++ harris_score.v
@@ -1,2 +1,36 @@
-module(input [7:0] Gx[0:3][0:3], input [7:0] Gy[0:3][0:3], output [7:0] R);
+module(input [7:0] Gx[0:3][0:3], input clk, input [7:0] Gy[0:3][0:3], output [32:0] R, output rdy);
+
+reg [7:0] Matrix[0:1][0:1];
+wire [8:0]  det, trace;
+
+
+genvar i;
+genvar j;
+
+always@(*) begin
+Matrix[0][0] <= 2'd0;
+Matrix[0][1] <= 2'd0;
+Matrix[1][0] <= 2'd0;
+Matrix[1][1] <= 2'd0;
+
+
+for (i=2'd0; i<=2'd3; i=i+2'd1) begin
+  for (j=2'd0; j<=2'd3; j=j+2'd1) begin
+  Matrix[0][0] = Gx[i][j]*Gx[i][j] + Matrix[0][0];
+  Matrix[0][1] = Gx[i][j]*Gy[i][j] + Matrix[0][1];
+  Matrix[1][0] = Gx[i][j]*Gy[i][j] + Matrix[1][0];
+  Matrix[1][1] = Gy[i][j]*Gy[i][j] + Matrix[1][1];
+  end
+end
+
+det = Matrix[0][0]*Matrix[1][1] - Matrix[0][1]*Matrix[1][0];
+trace = Matrix[0][0] + Matrix[1][1];
+end
+
+always@(posedge clk) begin
+R <= 
+rdy <= 'd1;
+  
+end
+
 endmodule
