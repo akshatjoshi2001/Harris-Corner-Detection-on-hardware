453e7d0e9bb6fc6b3c5b54efeff05183f8fc0978
Sarthak-22 <sarthakvora22@gmail.com>
2021-12-01 22:10:29 +0530

    Updated Harris Score

--- harris_score.v
+++ harris_score.v
@@ -16,15 +16,15 @@ Matrix[1][1] <= 2'd0;
 
 for (i=2'd0; i<=2'd3; i=i+2'd1) begin
   for (j=2'd0; j<=2'd3; j=j+2'd1) begin
-  Matrix[0][0] = Gx[i][j]*Gx[i][j] + Matrix[0][0];
-  Matrix[0][1] = Gx[i][j]*Gy[i][j] + Matrix[0][1];
-  Matrix[1][0] = Gx[i][j]*Gy[i][j] + Matrix[1][0];
-  Matrix[1][1] = Gy[i][j]*Gy[i][j] + Matrix[1][1];
+  Matrix[0][0] <= Gx[i][j]*Gx[i][j] + Matrix[0][0];
+  Matrix[0][1] <= Gx[i][j]*Gy[i][j] + Matrix[0][1];
+  Matrix[1][0] <= Gx[i][j]*Gy[i][j] + Matrix[1][0];
+  Matrix[1][1] <= Gy[i][j]*Gy[i][j] + Matrix[1][1];
   end
 end
 
-det = Matrix[0][0]*Matrix[1][1] - Matrix[0][1]*Matrix[1][0];
-trace = Matrix[0][0] + Matrix[1][1];
+det <= Matrix[0][0]*Matrix[1][1] - Matrix[0][1]*Matrix[1][0];
+trace <= Matrix[0][0] + Matrix[1][1];
 end
 
 always@(posedge clk) begin
